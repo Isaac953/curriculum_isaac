b953cbd4584b1abd1679ab1d65132578784d7fb9
Isaac Arias <isaac.arias07@gmail.com>
2024-02-07 23:58:32 -0600

    Nuevamente configurando

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: "export",
   basePath: "/cv_isaac",
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
+  reactStrictMode: true,
+  // images: {
+  //   unoptimized: true,
+  // },
 }
  
 module.exports = nextConfig

2a258051b5bfe0a19ef3d2dd39285ac3ad4670fc
Isaac Arias <isaac.arias07@gmail.com>
2024-02-07 19:43:41 -0600

    Prueba

--- src/app/components/cards/04_about/About.js
+++ src/app/components/cards/04_about/About.js
@@ -5,7 +5,7 @@ export const About = () => {
   const title = "ACERCA DE MÍ";
   const description ="Soy Ingeniero en Sistemas y Computación con experiencia en el área de programación de aplicaciones web \
    en el cual he trabajado en base a objetivos brindando soluciones de manera creativa y optima para empresas de tecnología, \
-    industriales y ONGs.";
+    industriales y ONGs";
   return (
     <section className="about-card">
       <h2>{title}</h2>
